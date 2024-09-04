56dbf2a0c996783e02c889761091560042cb3310
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2024-09-04 17:15:49 +0330

    create top ad banner

---
+++ components/shared/TopAdBanner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <VueImage :url="url" />
+  </div>
+</template>
+
+<script>
+import VueImage from "~/components/VueImage.vue";
+
+export default {
+  name: "TopAdBanner",
+  components: {VueImage},
+  data() {
+    return {
+      url: '/ad-service-banners/top-ad-banner.gif'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
