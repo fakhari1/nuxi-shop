82aeb5c110a3147a65a322efefd0ad9739ed25b1
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2024-09-04 17:14:59 +0330

    minor fix in main page

---
+++ components/layouts/mobile/MobileFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    this is mobile footer
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/MobileLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <MobileHeader/>
+  <div class="row">
+    <slot/>
+  </div>
+  <MobileFooter/>
+</template>
+
+<script>
+import MobileHeader from '~/components/layouts/mobile/MobileHeader.vue';
+import MobileFooter from '~/components/layouts/mobile/MobileFooter.vue';
+
+export default {
+  name: "Mobile",
+  components: [
+    MobileHeader,
+    MobileFooter
+  ],
+  data() {
+    return {}
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
