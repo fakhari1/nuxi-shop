63b1e65b671dc97fd997caeb131b7f749be4175a
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2024-09-04 18:35:41 +0330

    fix in top navigation bar

---
+++ components/shared/TopNavigationBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row py-1 px-5">
+    <VueImage :url="url" :style="{marginLeft: '10px', width: '200px'}"/>
+  </div>
+</template>
+
+<script>
+import VueImage from "~/components/VueImage.vue";
+
+export default {
+  name: "TopNavigationBar",
+  components: {
+    VueImage,
+  },
+  data() {
+    return {
+      url: '/logos/full-horizontal.svg'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
