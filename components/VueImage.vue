ea6d0153395673da7965593bc3e3e3fea43b86ec
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2024-09-04 17:15:30 +0330

    create vue image component

---
+++ components/VueImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="url" alt="" :style="{width: width, height: height}">
+</template>
+
+<script>
+export default {
+  name: 'VueImage',
+  props: {
+    url: {
+      required: true,
+      type: String,
+    },
+    width: {
+      default: '100%'
+    },
+    height: {
+      default: 'auto',
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
